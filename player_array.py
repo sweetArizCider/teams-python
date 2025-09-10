b1b40a2d248500f415e3bd7f2eb9a30490fabb9a
KVQN8BC_deere <ArizpeHernandezCarlos@JohnDeere.com>
2025-09-09 18:48:24 -0600

    feat :sparkles: applecider | enables players class as an array

--- player_array.py
+++ player_array.py
@@ -1,88 +1,90 @@
 class Player:
-  def __init__(self, name = '', age = 0, players = []):
-    self.players = players
-    self.name = name
-    self.age = age
+  def __init__(
+      self, 
+      name = None, 
+      age = None,  
+      number = None, #optional
+      nationality = None, #optional
+      position = None #optional
+    ):
+    # Name and Age are required to create an object!!
+    if not (name == None or age == None):
+      self.name = name
+      self.age = age
+      self.number = number
+      self.nationality = nationality
+      self.position = position
+      self.is_array = False
+      return
+    
+    self.is_array = True
+    self.players = []
     self.index = 0
     
-
   def __str__(self):
-    return f"Player: {self.name}, Age: {self.age}"
-    
-  def add_player(self, player):
-    if len(self.name) > 0 or self.age > 0:
-      return print("This is not an array of Players!")
-    self.players.append({
-      'name': player['name'], 
-      'age': player['age'], 
-      'index': self.index
-    })
-    self.index += 1
-    return print(f"New player added: Name: {player['name']}")
-
-  def remove_player(self, player_index):
-    if not self.players:
-      return print("There's no players to remove!")
-    if player_index >= len(self.players) or player_index < 0:
-      return print("Player index out of range!")
-    if len(self.name) > 0 or self.age > 0:
-      return print("This is not an array of Players!")
-    self.players.remove(self.players[player_index])
-
-  def update_player(self, new_name = '', new_age = 0, player_index = None):
-
-    if player_index is None:
-      if not self.players and (len(self.name) > 0 or self.age > 0):
-        if new_name:
-            self.name = new_name
-        if new_age:
-            self.age = new_age
-        return print("Single player updated!")
-      if self.players:
-        return print("This is not a single player!")
-      return print("Nothing to update!")
-
-    if player_index is not None:
-      if not self.players:
-        return print("There's no players to update!")
-      if player_index >= len(self.players) or player_index < 0:
-        return print("Player index out of range!")
-      if len(self.name) > 0:
-        self.players[player_index]['name'] = self.name
-      if self.age > 0:
-        self.players[player_index]['age'] = self.age
-      return print("Player updated!")
-
-  def show_player_list(self):
-    if not self.players:
-      return print("This is not an Array of Players!")
-    for player in self.players:
-      print(f"Player index: {player['index']} -> {player['name']}, Age: {player['age']}")
+    return f"Player: {self.name} \nAge: {self.age} \nNumber: {self.number} \nNationality: {self.nationality} \nPosition: {self.position}"
   
-  def show_player(self):
-    if len(self.name) > 0 or self.age > 0:
-      return print(f"Player: {self.name}, Age: {self.age}")
-    return print("This is not a single Player!")
+  def add_player(self, player):
+    if not self.is_array:
+      return print("This is not an Array of players!, player not added")
+    player.index = len(self.players)
+    self.players.append(player)
+    return print("Player added!")
+
+  def list_players(self):
+    if not self.is_array:
+      return print("This is not an Array of players!")
+    for player in self.players:
+      print(f"\n{player}")
+    return
+
+  def update_player(self, index, new_name = None, new_age = None, new_number = None, new_nationality = None, new_position = None):
+    if index == None:
+      return print("Index is required!!")
+    if not self.is_array:
+      return print("This is not an array of players!!")
+    player = self.players[index]
+
+    if new_name:
+      player.name = new_name
+    if new_age:
+      player.age = new_age
+    if new_number:
+      player.number = new_number
+    if new_nationality:
+      player.nationality = new_nationality
+    if new_position:
+      player.position = new_position
+    return print("Player updated!")
+
+  def remove_player(self, index):
+    if index == None:
+      return print("Index is required!!")
+    if not self.is_array:
+      return print("This is not an array of players!!")
+    self.players.pop(index)
+    return print("Player removed!")
 
 if __name__ == '__main__':
 
   # Creating a single player
   print("=================SINGLE PLAYER=================")
   single_player = Player('Carlos', 21)
-  single_player.show_player()
-  single_player.update_player(new_name='Pamela', new_age=19)
-  single_player.show_player()
+  single_player2 = Player('Pamela', 19, 16, 'Mexico', 'Goalkeeper')
+  single_player3 = Player('Jose', 32, 2, 'Mexico', 'Defensor')
+  print(single_player2)
 
   print("=================PLAYERS ARRAY=================")
-
-  # Creating an array of players
   players_array = Player()
-  players_array.add_player({'name': 'Carlos', 'age': 21})
-  players_array.add_player({'name': 'Pamela', 'age': 19})
-  players_array.show_player_list()
+  players_array.add_player(single_player2)
+  players_array.add_player(single_player)
+  players_array.add_player(single_player3)
 
-  # TODO fix the update_player method for arrays
-  players_array.update_player(new_name='Jose', new_age=22, player_index=0)
-  players_array.show_player_list()
+  players_array.update_player(index=0, new_name='Carlos A.', new_age=22, new_number=10, new_nationality='Mexico', new_position='Midfielder')
+  players_array.update_player(index=1, new_age=20)
+  players_array.remove_player(index=2)
+  players_array.list_players()
+
+  
 
   
