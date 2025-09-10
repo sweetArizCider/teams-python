7a14a6643969e5923231b93ad38a78fa96c452de
KVQN8BC_deere <ArizpeHernandezCarlos@JohnDeere.com>
2025-09-10 17:53:11 -0600

    chores: :work_in_progress: applecider | works on creating a object class to extent to players

--- player.py
+++ player.py
@@ -1,33 +1,47 @@
+from object import Object
+class Player(Object):
+  def __init__(
+    self, 
+    name = None, 
+    age = None,  
+    number = None, #optional
+    nationality = None, #optional
+    position = None #optional
+  ):
+    if not (name == None or name == None):
+      self.name = name
+      self.age = age
+      self.number = number if number is not None else None
+      self.nationality = nationality if nationality is not None else None
+      self.position = position if position is not None else None
+    else:
+      super().__init__()
 
-class Players:
-  def __init__(self, name: str, age: int, active: bool = True):
-    self.name = name
-    self.age = age
-    self.active = active
-
-  def __str__(self):
-    return f"{self.name}, {self.age}, active={self.active})"
-
-  def change_name(self, new_name: str):
-    if not new_name:
-      return
-    self.name = new_name
-
-  def change_age(self, new_age: int):
-    if not new_age:
-      return
-    self.age = new_age
-
-  def desactive(self):
-    self.active = False
-
-  def activate(self):
-    self.active = True
-
+  # TODO COMPLETE THE EXTENTION FROM OBJECT!!
+    
 if __name__ == '__main__':
-  carlos = Player('Carlos Arizpe', 21)
-  carlos.change_name('Carlos A.')
-  carlos.change_age(22)
-  carlos.desactive()
-  carlos.activate()
-  print(carlos)
+
+  # Creating a single player
+  print("=================SINGLE PLAYER=================")
+  single_player = Player('Carlos', 21)
+  single_player2 = Player('Pamela', 19, 16, 'Mexico', 'Goalkeeper')
+  single_player3 = Player('Jose', 32, 2, 'Mexico', 'Defensor')
+  single_player4 = Player('Luis', 45, 76)
+  print(single_player)
+
+
+  print("=================PLAYERS ARRAY=================")
+  players_array = Player()
+  players_array.add_player(single_player)
+  players_array.add_player(single_player2)
+  players_array.add_player(single_player3)
+  players_array.add_player(single_player4)
+
+  players_array.update_player(index=0, new_name='Carlos A.', new_age=22, new_number=10, new_nationality='Mexico', new_position='Midfielder')
+  players_array.update_player(index=1, new_age=20)
+  players_array.remove_player(index=2)
+  players_array.list_players()
+
+  
+
+  
--- team.py
+++ team.py
@@ -1,38 +1,81 @@
-
 class Team:
-  def __init__(self, name: str, sport: str, active: bool = True):
-    self.name = name
-    self.sport = sport
-    self.active = active
-
-  def __str__(self):
-    return f"{self.name}, {self.sport}, active={self.active})"
-
-  def change_name(self, new_name: str):
-    if not new_name:
+  def __init__(
+      self, 
+      name = None, 
+      sport = None,  
+      city = None, #optional
+    ):
+    # Name and Sport are required to create an object!!
+    if not (name == None or sport == None):
+      self.name = name
+      self.sport = sport
+      self.city = city
+      self.is_array = False
       return
-    old_team_name = self.name
-    self.name = new_name
+    
+    self.is_array = True
+    self.object_array = []
+    self.index = 0
+    
+  def __str__(self):
+    return f"Team: {self.name} \nSport: {self.sport} \nCity: {self.city}"
+
+  def add_team(self, team):
+    if not self.is_array:
+      return print("This is not an Array of teams!, team not added")
+    team.index = len(self.teams)
+    self.teams.append(team)
+    return print("Team added!")
+
+  def list_teams(self):
+    if not self.is_array:
+      return print("This is not an Array of teams!")
+    for team in self.teams:
+      print(f"\n{team}")
     return
 
-  def change_sport(self, new_sport: str):
-    if not new_sport:
-      return
-    self.sport = new_sport
+  def update_team(self, index, new_name = None, new_sport = None, new_city = None):
+    if index == None:
+      return print("Index is required!!")
+    if not self.is_array:
+      return print("This is not an array of teams!!")
+    team = self.teams[index]
 
-  def desactive(self):
-    self.active = False
+    if new_name:
+      team.name = new_name
+    if new_sport:
+      team.sport = new_sport
+    if new_city:
+      team.city = new_city
+    return print("Team updated!")
 
-  def activate(self):
-    self.active = True
-
-
+  def remove_team(self, index):
+    if index == None:
+      return print("Index is required!!")
+    if not self.is_array:
+      return print("This is not an array of teams!!")
+    self.teams.pop(index)
+    return print("Team removed!")
 
 if __name__ == '__main__':
-  cruz_azul = Team('Cruz Azul', 'Baseball')
-  cruz_azul.change_name('Cruz Azul FC')
-  cruz_azul.change_sport('Soccer')
-  cruz_azul.desactive()
-  cruz_azul.activate()
-  print(cruz_azul)
 
+  # Creating a single team
+  print("=================SINGLE TEAM=================")
+  single_team = Team('Cruz Azul', 'Soccer', 'Mexico City')
+  single_team2 = Team('Santos Laguna', 'Soccer', 'Torreon')
+  single_team3 = Team('Toluca', 'Soccer', 'Edo Mex')
+  single_team4 = Team('Vikings', 'Football', 'Minesotta')
+  print(single_team)
+
+
+  print("=================TEAMS ARRAY=================")
+  teams_array = Team()
+  teams_array.add_team(single_team)
+  teams_array.add_team(single_team2)
+  teams_array.add_team(single_team3)
+  teams_array.add_team(single_team4)
+
+  teams_array.update_team(index=0, new_name='Cruz Azul FC')
+  teams_array.update_team(index=3, new_city='Minneapolis')
+  teams_array.remove_team(index=2)
+  teams_array.list_teams()
